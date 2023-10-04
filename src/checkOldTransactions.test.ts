f633ee5fc9db896f953a4e166ae0d65ee9216bc9
Zexuz <robin.edbom@gmail.com>
2023-10-04 14:19:54 +0100

    Add sync for received transactions

--- src/checkOldTransactions.test.ts
+++ src/checkOldTransactions.test.ts
@@ -1,4 +1,4 @@
-import {getPendingTransactions, setTransactionAsCompleted,} from "./database/transactions";
+import {getPendingTransactions, setTransactionAsCompleted, getReceivedTransactions} from "./database/transactions";
 import {sleep} from "./utils/utils";
 import {retryOrFailTransaction} from "./retryOrFailTransaction";
 import {checkOldTransactions} from "./checkOldTransactions";
@@ -10,6 +10,7 @@ jest.mock('./retryOrFailTransaction');
 const mockGetPendingTransactions = getPendingTransactions as jest.MockedFunction<typeof getPendingTransactions>;
 const mockSetTransactionAsCompleted = setTransactionAsCompleted as jest.MockedFunction<typeof setTransactionAsCompleted>;
 const mockRetryOrFailTransaction = retryOrFailTransaction as jest.MockedFunction<typeof retryOrFailTransaction>;
+const mockGetReceivedTransactions = getReceivedTransactions as jest.MockedFunction<typeof getReceivedTransactions>;
 
 const mockSleep = sleep as jest.MockedFunction<typeof sleep>;
 
@@ -21,31 +22,52 @@ const mockProvider = {
 const mockWallet = {
   sendTransaction: jest.fn()
 }
+
+const mockQueue = {
+  enqueue: jest.fn(),
+}
+
 describe('checkOldTransactions', () => {
 
   beforeEach(() => {
     jest.clearAllMocks();
   });
 
+  it('should add received transactions to queue', async () => {
+    mockGetReceivedTransactions.mockResolvedValueOnce([
+        {eventId: 'txHash1-1', status: 'received', receivedAtBlock: 1} as any,
+        {eventId: 'txHash1-2', status: 'received', receivedAtBlock: 1} as any,
+      ]
+    );
+    mockGetPendingTransactions.mockResolvedValueOnce([]);
+
+    await checkOldTransactions(mockProvider as any, mockWallet as any, mockQueue as any);
+
+    expect(mockQueue.enqueue).toHaveBeenCalledTimes(2);
+  });
+
+
   it('should throw error if hash is not set', async () => {
+    mockGetReceivedTransactions.mockResolvedValueOnce([]);
     mockGetPendingTransactions.mockResolvedValueOnce([
         {eventId: '1', status: 'pending'} as any,
       ]
     );
 
-    expect(checkOldTransactions(mockProvider as any, mockWallet as any))
+    expect(checkOldTransactions(mockProvider as any, mockWallet as any, mockQueue as any))
       .rejects
       .toThrowError('Transaction hash not found');
   });
 
   it('should set already done transactions to completed', async () => {
+    mockGetReceivedTransactions.mockResolvedValueOnce([]);
     mockGetPendingTransactions.mockResolvedValueOnce([
         {eventId: '1', status: 'pending', txHash: '0xHash'} as any,
       ]
     );
     mockProvider.getTransactionReceipt.mockResolvedValue({status: 1});
 
-    await checkOldTransactions(mockProvider as any, mockWallet as any);
+    await checkOldTransactions(mockProvider as any, mockWallet as any, mockQueue as any);
 
     expect(mockGetPendingTransactions).toHaveBeenCalledTimes(1);
     expect(mockProvider.getTransactionReceipt).toHaveBeenCalledTimes(1);
@@ -54,6 +76,7 @@ describe('checkOldTransactions', () => {
 
 
   it('should update transaction to completed when done', async () => {
+    mockGetReceivedTransactions.mockResolvedValueOnce([]);
     mockGetPendingTransactions.mockResolvedValueOnce([
         {eventId: '1', status: 'pending', txHash: '0xHash'} as any,
       ]
@@ -61,7 +84,7 @@ describe('checkOldTransactions', () => {
     mockProvider.getTransactionReceipt.mockResolvedValueOnce(null).mockResolvedValueOnce({status: 1});
 
 
-    await checkOldTransactions(mockProvider as any, mockWallet as any);
+    await checkOldTransactions(mockProvider as any, mockWallet as any, mockQueue as any);
 
     expect(mockGetPendingTransactions).toHaveBeenCalledTimes(1);
     expect(mockProvider.getTransactionReceipt).toHaveBeenCalledTimes(2);
@@ -70,13 +93,14 @@ describe('checkOldTransactions', () => {
   });
 
   it('should retry transaction if it failed', async () => {
+    mockGetReceivedTransactions.mockResolvedValueOnce([]);
     mockGetPendingTransactions.mockResolvedValueOnce([
         {eventId: '1', status: 'pending', txHash: '0xHash'} as any,
       ]
     );
     mockProvider.getTransactionReceipt.mockResolvedValueOnce({status: -1});
 
-    await checkOldTransactions(mockProvider as any, mockWallet as any);
+    await checkOldTransactions(mockProvider as any, mockWallet as any, mockQueue as any);
 
     expect(mockRetryOrFailTransaction).toHaveBeenCalledTimes(1);
   });
--- src/checkOldTransactions.ts
+++ src/checkOldTransactions.ts
@@ -1,11 +1,17 @@
-import {ethers} from "ethers";
-import {getPendingTransactions, setTransactionAsCompleted} from "./database/transactions";
+import {ethers, Provider, Wallet} from "ethers";
+import {getPendingTransactions, getReceivedTransactions, setTransactionAsCompleted} from "./database/transactions";
 import {sleep} from "./utils/utils";
 import {retryOrFailTransaction} from "./retryOrFailTransaction";
+import {ThreadSafeQueue} from "./queue";
+import {PingEvent} from "./types";
 
-export async function checkOldTransactions(provider: ethers.Provider, wallet: ethers.Wallet) {
-
+export async function checkOldTransactions(provider: Provider, wallet: Wallet, queue: ThreadSafeQueue<PingEvent>) {
+  await checkReceivedTransaction(queue);
+  await checkPendingTransactions(provider, wallet);
+}
+async function checkPendingTransactions(provider: Provider, wallet: Wallet) {
   const pendingTransactions = await getPendingTransactions();
+  console.log(`Found ${pendingTransactions.length} pending transactions`)
 
   const promises = pendingTransactions.map(async (transaction) => {
     const {txHash, eventId} = transaction;
@@ -30,4 +36,15 @@ export async function checkOldTransactions(provider: ethers.Provider, wallet: et
   });
 
   await Promise.all(promises);
+}
+
+export async function checkReceivedTransaction(queue: ThreadSafeQueue<PingEvent>) {
+  const receivedTransactions = await getReceivedTransactions();
+  for (const transaction of receivedTransactions) {
+    await queue.enqueue({
+      blockNumber: transaction.receivedAtBlock,
+      transactionHash: transaction.eventId.split('-')[0],
+      index: Number(transaction.eventId.split('-')[1])
+    });
+  }
 }
