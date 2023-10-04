f498a8a34aac8e407afd65922eb788d492707ff8
Zexuz <robin.edbom@gmail.com>
2023-10-04 01:41:46 +0100

    Rename checkMissedEvents.test.ts -> processMissedEvents.ts

--- src/processMissedEvents.test.ts
+++ src/processMissedEvents.test.ts
@@ -1,13 +1,13 @@
 import {ethers} from "ethers";
 import {getMissedEvents} from "./getMissedEvents";
-import {checkMissedEvents} from "./checkMissedEvents";
+import {processMissedEvents} from "./processMissedEvents";
 
 
 jest.mock('./getMissedEvents');
 
 const mockedGetUnprocessedEvents = getMissedEvents as jest.MockedFunction<typeof getMissedEvents>;
 
-describe('checkMissedEvents', () => {
+describe('processMissedEvents', () => {
 
   it('should get missed events after block', async () => {
     mockedGetUnprocessedEvents.mockResolvedValueOnce([
@@ -16,7 +16,7 @@ describe('checkMissedEvents', () => {
       ]);
     const mockCallback = jest.fn();
 
-    await checkMissedEvents({} as any, (event) => mockCallback(event));
+    await processMissedEvents({} as any, (event) => mockCallback(event));
 
     expect(mockedGetUnprocessedEvents).toBeCalledTimes(1);
     expect(mockCallback).toBeCalledTimes(2);
--- src/processMissedEvents.ts
+++ src/processMissedEvents.ts
@@ -1,7 +1,7 @@
 import {Contract, ethers} from "ethers";
 import {getMissedEvents} from "./getMissedEvents";
 
-export async function checkMissedEvents(contract: Contract, callback: (event: ethers.EventLog | ethers.Log) => Promise<void>) {
+export async function processMissedEvents(contract: Contract, callback: (event: ethers.EventLog | ethers.Log) => Promise<void>) {
   const events = await getMissedEvents(contract);
 
   console.log(`Found ${events.length} events to process`);
